92e00dfc6d8501c38fbdc71ffa13976bacdd7fcd
Massyl Selmi <mass.selmi@gmail.com>
2020-05-26 21:00:29 +0200

    Tests sur les formes

---
+++ src/test/java/FormeTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import formes.Carre;
+import formes.Forme;
+import formes.Position2D;
+
+public class FormeTest {
+
+  Forme f = null;
+
+  @Before
+  public void setUp() throws Exception {
+    f = new Carre("carre", new Position2D(1, 1), 5);
+  }
+
+  @Test
+  public void testGetX() {
+    assertTrue(f.getPosition().getX() == 1);
+  }
+
+  @Test
+  public void testGetY() {
+    assertTrue(f.getPosition().getY() == 1);
+  }
+
+  @Test
+  public void testGetName() {
+    assertTrue(f.getName() == "carre");
+  }
+
+  @Test
+  public void testMove() {
+    f.move(2, 2);
+    assertTrue(f.getPosition().equals(new Position2D(3, 3)));
+  }
+
+}
